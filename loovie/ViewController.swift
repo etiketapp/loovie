3a180850ffeee5c8aeb5c83a6085ced1b9f3236b
tunay alver <tunay.alver@mobillium.com>
2019-10-24 02:32:27 +0300

    Initial Commit

---
+++ loovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  loovie
+//
+//  Created by tunay alver on 24.10.2019.
+//  Copyright © 2019 tunay alver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
